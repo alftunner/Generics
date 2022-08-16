97284dcd17abd14425603f5181b97ef6c074ca8b
alftunner <alftunner@gmail.com>
2022-08-16 21:27:04 +0300

    Initial commit

---
+++ Generics/OperationTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace Generics;
+
+internal sealed class OperationTimer : IDisposable
+{
+    private long _startTime;
+    private string _text;
+    private int _collectionCount;
+
+    public OperationTimer(string text)
+    {
+        PrepareForOperation();
+        _text = text;
+        _collectionCount = GC.CollectionCount(0);
+        _startTime = Stopwatch.GetTimestamp();
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"{_text}\t{(Stopwatch.GetTimestamp() - _startTime) / (double) Stopwatch.Frequency:0.00} " +
+                          $"секунды (сборок мусора {GC.CollectionCount(0) - _collectionCount})");
+    }
+
+    private static void PrepareForOperation()
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using Generics;
+
+const int COUNT = 10000000;
+
+using (new OperationTimer("List"))
+{
+    List<int> list = new List<int>(COUNT);
+    for (int i = 0; i < COUNT; i++)
+    {
+        list.Add(i);
+        int x = list[i];
+    }
+
+    list = null;
+}
+
+using (new OperationTimer("ArrayList"))
+{
+    ArrayList arrayList = new ArrayList();
+    for (int i = 0; i < COUNT; i++)
+    {
+        arrayList.Add(i);
+        int x = (int) arrayList[i];
+    }
+
+    arrayList = null;
+}
